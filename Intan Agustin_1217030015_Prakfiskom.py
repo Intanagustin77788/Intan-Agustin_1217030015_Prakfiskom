c6697deab3b26af454b1080de4367dc0f8e0f223
Intan Agustin <144421240+Intanagustin77788@users.noreply.github.com>
2023-09-22 13:11:12 +0700

    Add files via upload
    https://kumparan.com/berita-terkini/contoh-soal-momentum-dan-impuls-dan-jawabannya-1zoaov7RfYg/3

---
+++ Intan Agustin_1217030015_Prakfiskom.py
@@ -0,0 +1,21 @@
+#rumus perubahan momentum
+# diketahui suatu benda bermasa 2 kg dengan kecepatan awal 4 m/s kemudian ditumbuk sehingga menghasilkan kecepatan akhir 12 m/s dengan waktu tumbukan 2 sberapa gaya yang dihasilkan oleh benda tersebut
+# dit f ?
+t = 2 #s
+m = 2 #kg
+vi = 4 #m/s
+vf = 12
+# Rumus implus
+# I = f x t
+
+# rumus perubahan momentum
+Dv = vf - vi #Dv itu sama dengan delta v (perubahan kecepatan)
+
+F = m * Dv /t
+print (F, "N")
+
+
+
+
+
+ 
